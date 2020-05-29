fe8db4ec2dceb7c4ed80cef42abf08d11cd07288
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-29 08:55:00 -0400

    added label in searchbox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,13 +3,14 @@ import "./SearchBox.css";
 const SearchBox = ({searchfield, searchChange}) => {
     return(
         <div className="pa2">
-
-            <input 
-            className="pa3 searchbox"
-            type="search" 
-            placeholder="Search character here"
-            onChange={searchChange}
-            />
+            <label>
+                <input 
+                className="pa3 searchbox"
+                type="search" 
+                placeholder="Search character here"
+                onChange={searchChange}
+                />
+            </label>
 
         </div>
     )
